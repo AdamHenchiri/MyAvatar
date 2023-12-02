014fabdb279d98c501a8b495594a035f53b43a4a
adam henchiri <adam.henchiri@etu.umontpellier.fr>
2023-12-02 21:17:28 +0100

    init

---
+++ myAvatar/src/Controller/UtilisateurController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class UtilisateurController extends AbstractController
+{
+    #[Route('/', name: 'app_utilisateur', methods: ['GET'])]
+    public function index(): Response
+    {
+        return $this->render('utilisateur/index.html.twig', [
+            'controller_name' => 'UtilisateurController',
+        ]);
+    }
+
+    #[Route('/utilisateur', name: 'app_utilisateur')]
+    public function user(): Response
+    {
+        return $this->render('utilisateur/index.html.twig', [
+            'controller_name' => 'UtilisateurController',
+        ]);
+    }
+}
