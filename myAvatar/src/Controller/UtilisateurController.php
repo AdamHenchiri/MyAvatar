27c3f06b35112428290047a9bf16732b69c66f76
adam henchiri <adam.henchiri@etu.umontpellier.fr>
2023-12-03 00:06:23 +0100

    route signup with mailer

--- myAvatar/src/Controller/UtilisateurController.php
+++ myAvatar/src/Controller/UtilisateurController.php
@@ -14,14 +14,16 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Mime\Address;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Contracts\Translation\TranslatorInterface;
+use SymfonyCasts\Bundle\VerifyEmail\Exception\VerifyEmailExceptionInterface;
 
 class UtilisateurController extends AbstractController
 {
-//    private EmailVerifier $emailVerifier;
-//
-//    public function __construct(EmailVerifier $emailVerifier) {
-//        $this->emailVerifier = $emailVerifier;
-//    }
+    private EmailVerifier $emailVerifier;
+
+    public function __construct(EmailVerifier $emailVerifier) {
+        $this->emailVerifier = $emailVerifier;
+    }
 
     #[Route('/', name: 'app_home', methods: ['GET'])]
     public function index(): Response
@@ -119,6 +121,26 @@ class UtilisateurController extends AbstractController
     #[Route('/logout', name: 'app_user_logout', methods: ['GET'])]
     public function logout(): never {
         throw new \Exception('This should never be reached!');
+    }
+
+    #[Route('/verify/email', name: 'app_verify_email')]
+    public function verifyUserEmail(Request $request, TranslatorInterface $translator): Response
+    {
+        $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
+
+        // validate email confirmation link, sets User::isVerified=true and persists
+        try {
+            $this->emailVerifier->handleEmailConfirmation($request, $this->getUser());
+        } catch (VerifyEmailExceptionInterface $exception) {
+            $this->addFlash('verify_email_error', $translator->trans($exception->getReason(), [], 'VerifyEmailBundle'));
+
+            return $this->redirectToRoute('app_user_signup');
+        }
+
+        // @TODO Change the redirect on success and handle or remove the flash message in your templates
+        $this->addFlash('success', 'Your email address has been verified.');
+
+        return $this->redirectToRoute('app_home');
     }
 
 }
