f2b88bd9c3cb53afe45726886de5ff1abdec2094
adam henchiri <adam.henchiri@etu.umontpellier.fr>
2023-12-02 22:09:25 +0100

    add methode controller

--- myAvatar/src/Controller/UtilisateurController.php
+++ myAvatar/src/Controller/UtilisateurController.php
@@ -8,12 +8,10 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class UtilisateurController extends AbstractController
 {
-    #[Route('/', name: 'app_utilisateur', methods: ['GET'])]
+    #[Route('/', name: 'app_home', methods: ['GET'])]
     public function index(): Response
     {
-        return $this->render('utilisateur/index.html.twig', [
-            'controller_name' => 'UtilisateurController',
-        ]);
+        return $this->render('index.html.twig');
     }
 
     #[Route('/utilisateur', name: 'app_utilisateur')]
@@ -23,4 +21,63 @@ class UtilisateurController extends AbstractController
             'controller_name' => 'UtilisateurController',
         ]);
     }
+
+    #[Route('/user/profile/{id}', name: 'app_user_profile')]
+    public function profile(int $id, UtilisateurRepository $utilisateurRepository): Response {
+
+        $u = $utilisateurRepository->find($id);
+        if (!$u)
+            throw $this->createNotFoundException("L'utilisateur n'existe pas");
+
+        $loggedInUser = $this->getUser();
+
+        $isCurrentUser = $loggedInUser && $loggedInUser->getUserIdentifier() != $u->getUserIdentifier();
+
+        return $this->render('utilisateur/profile.html.twig', [
+            'controller_name' => 'UtilisateurController',
+            'utilisateur' => $u,
+            'isCurrentUser' => $isCurrentUser,
+        ]);
+    }
+
+    #[Route('/user/profile/{id}/edit', name: 'app_profile_edit')]
+    public function edit(UtilisateurRepository $repository, #[MapEntity] Utilisateur $utilisateur, Request $request, EntityManagerInterface $em, UtilisateurManagerInterface $utilisateurManager): Response {
+
+        if (!$utilisateur) {
+            throw $this->createNotFoundException('Utilisateur non trouvé.');
+        }
+
+
+        $form = $this->createForm(ModifierProfilType::class, $utilisateur);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $photoProfil = $form["fichierPhotoProfil"]->getData();
+            $utilisateurManager->processNewUtilisateur($utilisateur, $photoProfil);
+            $em->flush();
+            $this->addFlash('success', 'Votre profil a été modifié avec succès.');
+            return $this->redirectToRoute('app_user_profile', ['id' => $utilisateur->getId()]);
+        }
+
+        return $this->render('utilisateur/modifierProfil.html.twig', [
+            'controller_name' => 'UtilisateurController',
+            'form' => $form->createView(),
+            'utilisateur' => $utilisateur,
+        ]);
+    }
+
+
+    #[Route('/login', name: 'app_user_login')]
+    public function login(): Response {
+        return $this->render('auth/login.html.twig', [
+            'controller_name' => 'UtilisateurController',
+        ]);
+    }
+
+    #[Route('/logout', name: 'app_user_logout', methods: ['GET'])]
+    public function logout(): never {
+        throw new \Exception('This should never be reached!');
+    }
+
 }
